4ce5ca2abaf63e0d8a6c694099b21958518c03a9
Annefleur Hay <annefleurhay1@gmail.com>
2023-04-04 15:58:26 +0200

    git test commit

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -3,6 +3,7 @@ import { DrinkButtons } from "./components/DrinkButtons";
 const greeting =
   "Welkom Annefleur, ik ben trots op je met wat je allemaal aan het doen bent";
 const description = <p>This app is going to change your life.</p>;
+const test = "git test";
 
 export const App = () => {
   return (
@@ -11,6 +12,7 @@ export const App = () => {
       <h1>{greeting}</h1>
       <p>Hoihoi! Hier is je eerste app</p>
       {description}
+      {test}
     </div>
   );
 };
