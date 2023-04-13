b76c4c2421a089b980afbdad56374cf5e841e9aa
Annefleur Hay <annefleurhay1@gmail.com>
2023-04-13 13:03:00 +0200

    added drinkOne and drinkTwo

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { DrinkButtons } from "./components/DrinkButtons";
+import { coffee, tea } from "../utils/data";
 const greeting =
   "Welkom Annefleur, ik ben trots op je met wat je allemaal aan het doen bent";
 const description = <p>This app is going to change your life.</p>;
@@ -8,7 +9,7 @@ const test = "git test";
 export const App = () => {
   return (
     <div className="App">
-      <DrinkButtons />
+      <DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
       <h1>{greeting}</h1>
       <p>Hoihoi! Hier is je eerste app</p>
       {description}
