5570e07e71b5fb2736388e8154802833e05d96c2
Annefleur Hay <annefleurhay1@gmail.com>
2023-04-13 13:52:48 +0200

    onderdelen angepast aan de hand van opdracht

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -1,19 +1,13 @@
 import "./App.css";
 import { DrinkButtons } from "./components/DrinkButtons";
 import { coffee, tea } from "../utils/data";
-const greeting =
-  "Welkom Annefleur, ik ben trots op je met wat je allemaal aan het doen bent";
-const description = <p>This app is going to change your life.</p>;
-const test = "git test";
+const greeting = "Welcome to our cafe!";
 
 export const App = () => {
   return (
     <div className="App">
-      <DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
       <h1>{greeting}</h1>
-      <p>Hoihoi! Hier is je eerste app</p>
-      {description}
-      {test}
+      <DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
     </div>
   );
 };
